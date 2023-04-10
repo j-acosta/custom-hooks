1a42a490ddfb0882caeb5d692ca2b02607472829
Javier Acosta <javier.oscar.acosta@hotmail.com>
2023-04-10 20:22:40 -0300

    folder restructuring

---
+++ useTodos/todoReducer.js
@@ -0,0 +1,26 @@
+
+
+export const todoReducer = (intialState = [], action) => {
+
+    switch (action.type) {
+        case '[TODO] Add Todo':
+            return [ ...intialState,  action.payload ];
+
+        case '[TODO] Remove Todo':
+            return intialState.filter(todo => todo.id !== action.payload);  
+        
+        case '[TODO] Toggle Todo':
+            return intialState.map(todo => {
+                if(todo.id ===  action.payload){
+                    return {
+                        ...todo,
+                        done: !todo.done
+                    }
+                }
+                return todo;
+            });
+            
+        default:
+            return intialState;
+    }
+}
